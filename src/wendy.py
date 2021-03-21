2e8632fd56d57f5f6108c043cf5eb15f4663db8f
blulegume <mngreenforest@gmail.com>
2021-03-21 02:15:43 -0400

    finishing touches

--- src/wendy.py
+++ src/wendy.py
@@ -1,15 +1,18 @@
 import discord
 
 class Wendy(discord.Client):
-    def __init__(self, word_count):
+    def __init__(self, word_count, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.word_count = word_count
         self.windbag = None
 
     async def on_ready(self):
         print('Beep boop I have started')
 
-    async def on_message(message):
-        if len(message.content.split()) > self.word_count:
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+        elif len(message.content.split()) > self.word_count:
             await message.channel.send("Sir this is a Wendy's")
             self.windbag = message.author
             print(f'{message.author} sure is a windbag')
@@ -18,5 +21,4 @@ class Wendy(discord.Client):
             self.windbag = None
         else:
             self.windbag = None
-
 
